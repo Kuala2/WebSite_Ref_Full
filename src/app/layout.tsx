d7b55fc11bfbae41c29695ab35f12d5bf2e7a102
Kuala2 <Egort3310@gmail.com>
2026-07-20 16:53:21 +0300

    feat: add Yandex.Metrika counter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Playfair_Display, Manrope } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
@@ -40,6 +41,23 @@ export default function RootLayout({
       className={`${playfair.variable} ${manrope.variable} scroll-smooth antialiased`}
     >
       <body className="bg-brand-cream text-brand-dark flex flex-col min-h-[100svh] overflow-x-hidden">
+        <Script id="yandex-metrika" strategy="afterInteractive">
+          {`
+            (function(m,e,t,r,i,k,a){
+                m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+                k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+            })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=110888800', 'ym');
+        
+            ym(110888800, 'init', {ssr:true, clickmap:true, ecommerce:"dataLayer", referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+          `}
+        </Script>
+        <noscript>
+          <div>
+            <img src="https://mc.yandex.ru/watch/110888800" style={{ position: "absolute", left: "-9999px" }} alt="" />
+          </div>
+        </noscript>
         <Header />
         <main className="flex-grow">{children}</main>
         <Footer />
